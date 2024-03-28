e9f97fd1f07f0aca096473d1f252b9495046c842
Andersson <andera29@sc1nc004is02.eth.rsiec.sc1.science.roche.com>
2024-03-28 11:41:39 -0700

    try 2

--- docs/conf.py
+++ docs/conf.py
@@ -7,29 +7,26 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
-sys.path.insert(0, os.path.abspath("../../src/"))
-
-
-project = "telegraph"
-copyright = "2024, BRAID-MAGIC"
-author = "BRAID-MAGIC"
-release = "beta-v1"
+project = 'telegraph'
+copyright = '2024, BRAID-MAGIC'
+author = 'BRAID-MAGIC'
+release = 'beta-v1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages",
-    "sphinx.ext.napoleon",
-]
+
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',]
 
 # Napoleon settings
 napoleon_google_docstring = True
@@ -44,12 +41,13 @@ napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
 
-templates_path = ["_templates"]
-exclude_patterns = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "default"
-html_static_path = ["_static"]
+html_theme = 'default'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. telegraph documentation master file, created by
+   sphinx-quickstart on Thu Mar 28 11:34:47 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to telegraph's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   source/telegraph
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
