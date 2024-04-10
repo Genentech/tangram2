527f612d5daa8675df6c5011567091db9710a287
Andersson <andera29@sc1nc001is01.eth.rsiec.sc1.science.roche.com>
2024-04-09 17:07:18 -0700

    var fix

--- src/telegraph/methods/utils.py
+++ src/telegraph/methods/utils.py
@@ -453,7 +453,7 @@ def merge_input_dicts(
     join_style_dict = {key: join_style for key in union_keys}
     if special_join_style is not None:
         for key, val in special_join_style.items():
-            join_styl_dict[key] = val
+            join_style_dict[key] = val
 
     for key in union_keys:
         obj_list = list()
